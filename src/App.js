dd0191541f4793ac8d8e82191781a4a8a0d352a8
Siva G <sivagnair2002@gmail.com>
2025-03-15 19:50:51 +0530

    Add Front component with animated image and letters, and update routing in App.js

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import ForgotPassword from './components/ForgotPassword';
 import ResetPassword from './components/ResetPassword';
 import Home from './components/Home';
 import './App.css';
+import Front from './components/Front';
 
 function App() {
   return (
     <Router>
       <div className="mobile-viewport">
         <Routes>
+          <Route path="/front" element={<Front />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
